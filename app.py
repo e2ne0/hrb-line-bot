fb967cb2143dc82819952308c1b9516ee99186c9
e2ne0 <e2ne00@gmail.com>
2019-12-16 22:44:54 +0800

    test build

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text == '追蹤中職缺':
-        follow()
+        line_bot_api.reply_message(event.reply_token, follow())
     
     
 
@@ -155,7 +155,8 @@ def follow():
             ]
         )
     )
-    line_bot_api.reply_message(event.reply_token, message)
+    return message
+    
 
 
 if __name__ == "__main__":
