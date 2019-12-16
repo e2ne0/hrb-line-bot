0e714e358b465c14610f97f3eebd7284af9ca5a9
e2ne0 <e2ne00@gmail.com>
2019-12-16 22:22:11 +0800

    test build

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
-)
+from linebot.models import *
 
 app = Flask(__name__)
 
@@ -44,75 +42,57 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    global questNum
-    global tip
-    global start
-    ans = ['4581', '2486', '5206', '5340', '1235']
-    questsURL = [
-        'https://imgur.com/osHNXd9.jpg',
-        'https://imgur.com/h6YyNbY.jpg',
-        'https://imgur.com/RHEUTRx.jpg',
-        'https://imgur.com/Ql0wB4b.jpg',
-        'https://imgur.com/xXJCZes.jpg'
-    ]
-
-    tipsURL = [
-        ['https://imgur.com/huqtjzT.jpg', 'https://imgur.com/NB4O7In.jpg'],
-        ['https://imgur.com/fGMbFLX.jpg', 'https://imgur.com/OMuYolZ.jpg'],
-        ['https://imgur.com/WNqv9rA.jpg', 'https://imgur.com/tp7S2Tn.jpg'],
-        ['https://imgur.com/wwOojCl.jpg', 'https://imgur.com/B6Y4FjF.jpg'],
-        ['https://imgur.com/SpsRdgy.jpg', 'https://imgur.com/7aMAHiq.jpg']
-    ]
-    if event.message.text == 'Start':
-        questNum = 0
-        tip = 0
-        message = ImageSendMessage(
-            original_content_url = questsURL[questNum],
-            preview_image_url = questsURL[questNum]
-        )
-        line_bot_api.reply_message(event.reply_token, message)
-        start = True
-        return
-    if start:
-        if event.message.text == 'tip' or event.message.text == 'Tip':
-            if tip < len(tipsURL[questNum]):
-                message = ImageSendMessage(
-                    original_content_url = tipsURL[questNum][tip],
-                    preview_image_url = tipsURL[questNum][tip]
+    message = TemplateSendMessage(
+        alt_text='Carousel template',
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url='https://example.com/item1.jpg',
+                    title='this is menu1',
+                    text='description1',
+                    actions=[
+                        PostbackAction(
+                            label='postback1',
+                            display_text='postback text1',
+                            data='action=buy&itemid=1'
+                        ),
+                        MessageAction(
+                            label='message1',
+                            text='message text1'
+                        ),
+                        URIAction(
+                            label='uri1',
+                            uri='http://example.com/1'
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://example.com/item2.jpg',
+                    title='this is menu2',
+                    text='description2',
+                    actions=[
+                        PostbackAction(
+                            label='postback2',
+                            display_text='postback text2',
+                            data='action=buy&itemid=2'
+                        ),
+                        MessageAction(
+                            label='message2',
+                            text='message text2'
+                        ),
+                        URIAction(
+                            label='uri2',
+                            uri='http://example.com/2'
+                        )
+                    ]
                 )
-                line_bot_api.reply_message(event.reply_token, message)
-                tip += 1
-                return
-            else:
-                message = TextSendMessage(text="已經用了所有提示了，\n再努力想想吧！")
-                line_bot_api.reply_message(event.reply_token, message)
-                return
-
-        if event.message.text == ans[questNum]:
-            if questNum == len(ans) - 1:
-                start = False
-                questNum = 0
-                tip = 0
-                message = TextSendMessage(text="答對了，挑戰結束！")
-                line_bot_api.reply_message(event.reply_token, message)
-                return
-            
-            tip = 0
-            questNum += 1
-            message = [TextSendMessage(text="答對了，下一題！"),ImageSendMessage(
-            original_content_url = questsURL[questNum],
-            preview_image_url = questsURL[questNum]
-            )]
-            line_bot_api.reply_message(event.reply_token, message)
-            return
-        for i in range(0,questNum + 1):
-            if ans[i] == event.message.text:
-                message = TextSendMessage(text="這是別題的答案吧")
-                line_bot_api.reply_message(event.reply_token, message)
-                return
-        
-    message = TextSendMessage(text="請輸入Start開始遊戲\n tip獲取提示\n 或是直接輸入答案")
+            ]
+        )
+    )
     line_bot_api.reply_message(event.reply_token, message)
+
+def follow():
+    return
 
 
 if __name__ == "__main__":
