1af005bfefad378777ec27a81569caf028cfb529
e2ne0 <e2ne00@gmail.com>
2019-12-17 21:26:44 +0800

    First step of Rich-menu, powered by shell command....

---
+++ upload-image-to-rich-menu.sh
@@ -0,0 +1,5 @@
+curl -v -X POST https://api.line.me/v2/bot/richmenu/richmenu-e5973fe50a153e8c3e4418109c0b6573/content \
+-H "Authorization: Bearer 6yNy7TqSZDYbYo98fd2M0nZ9tMyXxaeQvtKMWo/+YOroOUwM0ghQgAHqRrDDEwxJ7XvCG2FS/+k4yNev17o5vQFlgQw8qT8rmA6j/us4lw6kHECLAzT/CaboZbTYqumbFnfHMoEWOXVXpeJcP0FiUwdB04t89/1O/w1cDnyilFU=' \
+-H 'Content-Type: application/json" \
+-H "Content-Type: image/png" \
+-T image-path.png
